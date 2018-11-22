5bc23b5ea0a99abd168763b35eebcb9ebc51faf1
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-11-22 01:52:43 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,56 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #return an array of hashes
+    #hash rep a single student card
+    #std hashes ex: {:name , :location, :profile_url}
+    # .css(value)
+    # .attribute
+
+    html = Nokogiri::HTML(open(index_url))
+    student_cards = html.css("div.student-card")
+    # student_cards = html.css(".student-card")
+    student_cards.collect do |card|
+      hash = {
+          name: card.css('.student-name').text,
+          location: card.css('.student-location').text,
+          # another way to get profile_url value: card.css('a').attr('href').text
+          profile_url: card.css('a').attribute('href').value
+      }
+      hash
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    #returns a hash like below vvv
+    # {:twitter=>"http://twitter.com/flatironschool",
+    #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+    #   :github=>"https://github.com/learn-co,
+    #   :blog=>"http://flatironschool.com",
+    #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+    #   :bio=> "I'm a school"
+    #  }
+
+    html = Nokogiri::HTML(open(profile_url))
+
+    hash = {}
+    # binding.pry
+    hash[:profile_quote] = html.css('.vitals-text-container .profile-quote').text
+    hash[:bio] = html.css('.description-holder p').text
+    links = html.css('.social-icon-container a').map {|nodeset| nodeset.attribute('href').value }
+
+    links.each do |link|
+      if link.include?('twitter')
+        hash[:twitter] = link
+      elsif link.include?('linkedin')
+        hash[:linkedin] = link
+      elsif link.include?('github')
+        hash[:github] = link
+      else
+        hash[:blog] = link
+      end
+    end
+    hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+      #Send is Calling Student Object attribute setter method
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+        self.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value )
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
